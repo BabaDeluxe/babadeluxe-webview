b789c95a189f25eb9382f21c5485942c8ea5aaae
simwai <16225108+simwai@users.noreply.github.com>
2026-04-18 14:52:23 +0200

    chore: :wrench: Updated deployment scripts to be more robust

--- redeploy.sh
+++ redeploy.sh
@@ -1,2 +1,5 @@
-#!/bin/bash
-pm2 stop babadeluxe-webview-auto-deploy && pm2 delete babadeluxe-webview-auto-deploy && pm2 start ecosystem.config.js && pm2 logs babadeluxe-webview-auto-deploy
+#!/usr/bin/env bash
+pm2 stop babadeluxe-webview-auto-deploy || true
+pm2 delete babadeluxe-webview-auto-deploy || true
+pm2 start ecosystem.config.js
+pm2 logs babadeluxe-webview-auto-deploy
