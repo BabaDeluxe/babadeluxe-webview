8bb07d5319a2c2790bbda93ae92a8d8e8db89191
simwai <16225108+simwai@users.noreply.github.com>
2026-04-26 12:44:29 +0200

    chore: :wrench: Updated woodpecker pipeline and pre-commit-script

--- redeploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-pm2 stop babadeluxe-backend-auto-deploy 2>/dev/null || true
-pm2 delete babadeluxe-backend-auto-deploy 2>/dev/null || true
-pm2 start ecosystem.config.js
-pm2 logs babadeluxe-backend-auto-deploy
